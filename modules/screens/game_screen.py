b2dc7d5876ca0004d3f4d36d5894172a5de73138
Nguyen Phuoc Hung <zakizika1590623847@gmail.com>
2025-12-06 00:18:05 +0700

    Integrate level_manager into game_screen

--- modules/screens/game_screen.py
+++ modules/screens/game_screen.py
@@ -7,15 +7,15 @@ from modules.managers.level_manager import LevelManager
 class GameScreen:
     def __init__(self, screen, sound_manager):
         self.screen = screen
+        self.sound_manager = sound_manager  # Thêm dòng này để gán param vào instance
         self.background_color = COLOR_GREEN
-        self.player = Player(sound_manager)
-        self.level_manager = LevelManager()
+        self.player = Player(self.sound_manager)
+        self.level_manager = LevelManager(self.sound_manager)
         self.player.enemies = self.level_manager.enemies
         self.resources = []
-        # self.sound_manager = sound_manager
-        # self.sound_manager.play_music('music_wave.mp3')
 
     def update(self, delta_time, keys):
+        """Update entities với delta_time và keys."""
         self.player.update(delta_time, keys)
         self.level_manager.update(delta_time, self.player)
 
@@ -27,9 +27,10 @@ class GameScreen:
         self.player.dropped_resources.clear()
 
         if not self.player.invincible:
-            self.player.take_damage(0.1)  # Test, remove later
+            self.player.take_damage(0.1)  # Test HP giảm chậm
 
     def draw_background(self):
+        """Draw background và entities."""
         self.screen.fill(self.background_color)
         self.level_manager.draw(self.screen)
         self.player.draw(self.screen)
